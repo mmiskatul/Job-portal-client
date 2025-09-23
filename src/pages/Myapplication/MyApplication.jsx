301ea6461aef6c30ee8972f0f47d547e271ff379
mmiskatul <masabimiskat@gmail.com>
2025-09-23 13:18:51 +0600

    add new component application list

--- src/pages/Myapplication/MyApplication.jsx
+++ src/pages/Myapplication/MyApplication.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ApplicationStat from './ApplicationStat'
 
 const MyApplication = () => {
   return (
-    <div>
-            
+    <div className='flex items-center justify-around'>
+            <ApplicationStat></ApplicationStat>
     </div>
   )
 }
