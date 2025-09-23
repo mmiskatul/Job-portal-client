9c0b2e0e6266e773283da7df6854b9506a5d0a44
mmiskatul <masabimiskat@gmail.com>
2025-09-23 13:44:26 +0600

    add application list

--- src/pages/Myapplication/MyApplication.jsx
+++ src/pages/Myapplication/MyApplication.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ApplicationStat from './ApplicationStat'
+import ApplicationList from './ApplicationList'
+import useAuth from '../../hooks/useAuth'
 
 const MyApplication = () => {
+    const {user}=useAuth();
   return (
     <div className='flex items-center justify-around'>
             <ApplicationStat></ApplicationStat>
+           <Suspense fallback={'loading your Applications '}>
+             <ApplicationList myApplicationsPromise={myApplicationsPromise(user.email)}/>
+           </Suspense>
     </div>
   )
 }
